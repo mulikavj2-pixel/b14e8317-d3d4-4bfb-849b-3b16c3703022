3715c6962bcf2561c9d42a6688c8a512c5191098
mulikavj2-pixel <mulikavj2@gmail.com>
2025-12-17 15:49:22 +0200

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,9 +50,9 @@ export default function BrewHavenPage() {
             { text: "Explore Our Menu", href: "menu" },
             { text: "Learn Our Story", href: "about" }
           ]}
-          showcaseImageSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_30ordogEOn7VO6LZdIWb5atLXky/uploaded-1765979322234-4sh9zx23.jpg"
+          showcaseImageSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_30ordogEOn7VO6LZdIWb5atLXky/uploaded-1765979357205-0khe7tvn.jpg"
           showcaseImageAlt="Barista creating latte art"
-          imageSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_30ordogEOn7VO6LZdIWb5atLXky/uploaded-1765979322234-4sh9zx23.jpg"
+          imageSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_30ordogEOn7VO6LZdIWb5atLXky/uploaded-1765979357205-0khe7tvn.jpg"
           imageAlt="Coffee shop ambiance"
           showDimOverlay={true}
         />
